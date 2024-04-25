e839fe197deeaad86dc4549ff778022bd4404893
Versart <wesleyversart@gmail.com>
2024-04-25 16:34:51 -0300

    feat: Add Form
    Adds form

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    let form = document.getElementById('register_form');
+    if(mode.classList.contains('fa-moon')){
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+        form.classList.add('dark');
+        return;
+    }
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon');
+    form.classList.remove('dark');
+    
+});
+
